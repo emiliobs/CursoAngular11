27257a059e352a530c06f8b1322da478a5f6b4f1
Emilio Barrea <barrera_emilio@yahoo.es>
2021-03-13 11:56:21 +0000

    Two Way Data BIndig
    What is two-way Databinding?
    How to use ngModel directive to achieve it.

--- src/app/my-comp/my-comp.component.ts
+++ src/app/my-comp/my-comp.component.ts
@@ -6,26 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-comp.component.css']
 })
 export class MyCompComponent implements OnInit {
+  public textValue: String = "";
 
-  myText: string = "Tis is my Text: Emilio";
-  myInputText : string = "";
-  MyText: string = "init";
 
-  changeText = () =>
-  {
-      this.myText = "This text was changes for a new one!!!! - Amiliana";
-  };
-
-  chageEvent = (event:any) =>
-  {
-    console.log(event);
-    console.log(event.srcElement.innerText)
-    event.srcElement.innerText = "This has been changed";
-  };
-
-  onKeyPressed = (event:any) => {
-   this.myInputText += event.target.value + '  && ';
-  };
 
 constructor() { }
 
